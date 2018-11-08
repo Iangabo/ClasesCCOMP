e1b962197dbf76cacbfeb96d6926976a59d7aef9
Iangabo <31105000+Iangabo@users.noreply.github.com>
2018-11-08 09:05:16 -0500

    Update Perro.cpp

--- Perro.cpp
+++ Perro.cpp
@@ -1,1 +1,13 @@
 
+
+void Perro::color(string x)
+{
+    cout<<"el color del gato es:"<<x<<"\n";
+}
+
+void gato::habla()
+{
+        cout<<"su nombre es: "<<nombre<<"\n";
+        cout<<"tiene "<<nroPatas<<" patas\n";
+        cout<<"no habla\n";
+}
