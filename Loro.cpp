e8c229b51f51b5da0af18b648f32699b4b6e0eb8
Iangabo <31105000+Iangabo@users.noreply.github.com>
2018-11-08 09:04:36 -0500

    Update Loro.cpp

--- Loro.cpp
+++ Loro.cpp
@@ -1,1 +1,12 @@
 
+void Loro::pico(string x)
+{
+    cout<<"su pico es:"<<x;
+}
+
+void gato::habla()
+{
+        cout<<"Se llama: "<<nombre;
+        cout<<"tiene "<<nroPatas<<" patas";
+        cout<<"gruacccc!";
+}
