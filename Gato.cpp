07872b406d4d48ae0b242d57d48a16fdc84022ef
Iangabo <31105000+Iangabo@users.noreply.github.com>
2018-11-08 09:02:46 -0500

    Update Gato.cpp

--- Gato.cpp
+++ Gato.cpp
@@ -1,1 +1,12 @@
 
+void gato::unias(string x)
+{
+    cout<<"sus unias son de color:"<<x<<"\n";
+}
+
+void gato::habla()
+{
+        cout<<"se llama: "<<nombre<<;
+        cout<<"tiene "<<nroPatas<<" patas";
+        cout<<"Miau!";
+}
